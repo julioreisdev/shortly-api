3bf7c3c5f33cc38bff7bccdcf62d610b273a3744
julioreisdev <juliosteam0352@gmail.com>
2022-08-02 16:24:31 -0300

    Adiciona controller para login (signIn)

--- src/controllers/signControllers.js
+++ src/controllers/signControllers.js
@@ -1,6 +1,8 @@
 import connection from "../databases/postgresql.js";
 import userSchema from "../schemas/userSchema.js";
+import loginSchema from "../schemas/loginSchema.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
   const { error } = userSchema.validate(req.body, { abortEarly: false });
@@ -36,5 +38,33 @@ export async function signUp(req, res) {
 }
 
 export async function signIn(req, res) {
-  return res.send("BOA demais");
+  const { error } = loginSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const erros = error.details.map((d) => d.message);
+    return res.status(422).send(erros);
+  }
+  const { email, password } = req.body;
+  try {
+    const { rows: user } = await connection.query(
+      `
+      SELECT * FROM users WHERE email = $1
+    `,
+      [email]
+    );
+    const userObject = user[0];
+    if (user && bcrypt.compareSync(password, userObject.password)) {
+      const token = uuid();
+      await connection.query(
+        `
+        INSERT INTO sessions ("userId", token)
+        VALUES ($1, $2)
+      `,
+        [userObject.id, token]
+      );
+      return res.send({ token });
+    }
+    return res.sendStatus(401);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 }
